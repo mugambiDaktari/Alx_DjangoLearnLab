9d114e41ab2d74d428b81d0614f56e963d8326cf
Cliff_daktari <wycliffmutethia@gmail.com>
2025-02-20 14:23:17 +0300

    Changed how we get list of all books

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,7 +4,7 @@ from relationship_app.models import Book, Author, Library, Librarian
 books_by_author = Book.objects.filter(author__name="Author's Name")
 
 # List all books in a library.
-library = Library.objects.get(name="Library Name")
+library = Library.objects.get(name="Library_name")
 books_in_library = library.books.all()
 
 # Retrieve the librarian for a library.
