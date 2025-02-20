e52c73d7829d2972a03978c10dbc1cd9b3a6897c
Cliff_daktari <wycliffmutethia@gmail.com>
2025-02-20 14:33:48 +0300

    deploged author_name variable

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,9 @@
 from relationship_app.models import Book, Author, Library, Librarian
 
 # Query all books by a specific author.
-books_by_author = Book.objects.filter(author__name="Author's Name")
+author_name = "Author's Name"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
 
 # List all books in a library.
 library_name = "Library_name"
