e892c8ec537e835b75d05bccbf6b12e05a844b5a
Cliff_daktari <wycliffmutethia@gmail.com>
2025-02-20 17:06:07 +0300

    added unnecessary relationship directory

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from .models import Author, Book, Library, Librarian
-from django.http import HttpResponse
 from django.views.generic import DetailView, ListView
 
 
 # Create your views here
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
-
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
