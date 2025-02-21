4af100b2ad001f346099bcb836520641445f67e2
Cliff_daktari <wycliffmutethia@gmail.com>
2025-02-21 18:50:02 +0300

    imported built-in view not yet used

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.models import User
-
+from django.contrib.auth import login
+from django.contrib.auth import logout
 
 # Create your views here
 def list_books(request):
