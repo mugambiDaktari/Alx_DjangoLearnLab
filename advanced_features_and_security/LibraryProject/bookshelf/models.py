bab42443ccd7401085745f9640a87afa46683013
mugambiDaktari <wycliffmutethia@gmail.com>
2025-02-27 16:44:05 +0300

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -46,6 +46,14 @@ class Book(models.Model):
        title = models.CharField(max_length=200)
        author = models.CharField(max_length=100)
        publication_year = models.IntegerField()
-
+        
+       class Meta:
+            permissions = [
+        ("can_view_book", "Can view book"),
+        ("can_create_book", "Can create book"),
+        ("can_edit_book", "Can edit book"),
+        ("can_delete_book", "Can delete book"),
+        ]
+       
        def __str__(self):
              return f"{self.title} by {self.author} ({self.publication_year})"
