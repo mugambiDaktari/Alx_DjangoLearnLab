4d236f2aa8c4fb6df281e460b7e25f2d47ec33b5
mugambiDaktari <wycliffmutethia@gmail.com>
2025-03-06 17:21:59 +0300

    used generics.ListAPIView

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,6 +5,6 @@ from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
