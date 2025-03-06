1eb2865bca01bf6f6d22d2b407a5d570d6bb4822
mugambiDaktari <wycliffmutethia@gmail.com>
2025-03-06 16:36:38 +0300

    Setting Up a New Django Project with Django REST Framework

---
+++ api_project/api_project/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    year_published = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
