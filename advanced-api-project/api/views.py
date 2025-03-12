3d5a27633179d7d14960e845e47ea50fd8e58219
mugambiDaktari <wycliffmutethia@gmail.com>
2025-03-12 18:40:20 +0300

    reordered my gamble line

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework import status
