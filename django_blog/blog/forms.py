995fbd218b66cb11d81ee0d6dfe14b9e755111d9
mugambiDaktari <wycliffmutethia@gmail.com>
2025-03-19 14:58:20 +0300

    added a TagWidget

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Post, Comment
+from taggit.forms import TagWidget  # Import TagWidget
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -25,7 +26,10 @@ class ProfileUpdateForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']
+        fields = ['title', 'content', 'tags']  # Include tags field
+        widgets = {
+            'tags': TagWidget(),  # Add TagWidget for better UI
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
