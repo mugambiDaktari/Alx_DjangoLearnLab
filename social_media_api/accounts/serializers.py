26edd27b5b7640928c721a54599e391e65ad996e
mugambiDaktari <wycliffmutethia@gmail.com>
2025-03-24 15:39:20 +0300

    Alx crazy requirement

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,11 +7,38 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user( 
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+        )
+        Token.objects.create(user=user)  # Generate token for new user
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+"""
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
         extra_kwargs = {'password': {'write_only': True}}
  
     def create(self, validated_data):
-        """Creates a user and ensures password is hashed."""
+        #Creates a user and ensures password is hashed.
         user = User.objects.create_user(  # Explicitly calling create_user()
             username=validated_data['username'],
             email=validated_data.get('email', ''),
@@ -24,3 +51,4 @@ class UserSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(write_only=True)
+    """
