7f17b5b91a9733b0c893baace8a646adbfa5075e
mugambiDaktari <wycliffmutethia@gmail.com>
2025-03-24 21:53:44 +0300

    Implementing Posts and Comments Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import PostSerializer, CommentSerializer
+from .models import Post, Comment
+
 
 # Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+        """   def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        return super().perform_create(serializer) """
+    
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]    
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+   
+    
