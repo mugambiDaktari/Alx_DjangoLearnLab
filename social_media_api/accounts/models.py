5d7e8b598f282288e3665d48052b2b77a93f2e87
mugambiDaktari <wycliffmutethia@gmail.com>
2025-03-24 20:19:06 +0300

    a random commit

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
     profile_picture = models.ImageField(null=True, blank=True)
     followers = models.ManyToManyField('self', related_name='following', symmetrical=False)
